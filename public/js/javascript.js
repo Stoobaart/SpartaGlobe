aa868870bf4e4f36401f33bbb6336abedebf1732
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-03-10 17:07:46 +0000

    Got the map to append to the body

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,68 +1,76 @@
-var width = 960,
-    height = 960;
+document.addEventListener("DOMContentLoaded", function() {
 
-var projection = d3.geo.orthographic()
-    .translate([width / 2, height / 2])
-    .scale(width / 2 - 20)
-    .clipAngle(90)
-    .precision(0.6);
+  var width = 960,
+      height = 960;
 
-var canvas = d3.select("body").append("canvas")
-    .attr("width", width)
-    .attr("height", height);
+  var projection = d3.geo.orthographic()
+      .translate([width / 2, height / 2])
+      .scale(width / 2 - 20)
+      .clipAngle(90)
+      .precision(0.6);
+      
+  var canvas = d3.select("body").append("canvas")
+      .attr("width", width)
+      .attr("height", height);
 
-var c = canvas.node().getContext("2d");
+  //var canvas = d3.select("globe_canvas");
+  console.log(d3)
+  console.log(canvas);
 
-var path = d3.geo.path()
-    .projection(projection)
-    .context(c);
+  var c = canvas.node().getContext("2d");
 
-var title = d3.select("h1");
+  var path = d3.geo.path()
+      .projection(projection)
+      .context(c);
 
-queue()
-    .defer(d3.json, "../globe-data/world-110m.json")
-    .defer(d3.tsv, "../globe-data/world-country-names.tsv")
-    .await(ready);
+  var title = d3.select("h1");
 
-function ready(error, world, names) {
-  if (error) throw error;
+  queue()
+      .defer(d3.json, "../globe-data/world-110m.json")
+      .defer(d3.tsv, "../globe-data/world-country-names.tsv")
+      .await(ready);
 
-  var globe = {type: "Sphere"},
-      land = topojson.feature(world, world.objects.land),
-      countries = topojson.feature(world, world.objects.countries).features,
-      borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }),
-      i = -1,
-      n = countries.length;
+  function ready(error, world, names) {
+    if (error) throw error;
 
-  countries = countries.filter(function(d) {
-    return names.some(function(n) {
-      if (d.id == n.id) return d.name = n.name;
+    var globe = {type: "Sphere"},
+        land = topojson.feature(world, world.objects.land),
+        countries = topojson.feature(world, world.objects.countries).features,
+        borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }),
+        i = -1,
+        n = countries.length;
+
+    countries = countries.filter(function(d) {
+      return names.some(function(n) {
+        if (d.id == n.id) return d.name = n.name;
+      });
+    }).sort(function(a, b) {
+      return a.name.localeCompare(b.name);
     });
-  }).sort(function(a, b) {
-    return a.name.localeCompare(b.name);
-  });
 
-  (function transition() {
-    d3.transition()
-        .duration(1250)
-        .each("start", function() {
-          title.text(countries[i = (i + 1) % n].name);
-        })
-        .tween("rotate", function() {
-          var p = d3.geo.centroid(countries[i]),
-              r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
-          return function(t) {
-            projection.rotate(r(t));
-            c.clearRect(0, 0, width, height);
-            c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill();
-            c.fillStyle = "#f00", c.beginPath(), path(countries[i]), c.fill();
-            c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
-            c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
-          };
-        })
-      .transition()
-        .each("end", transition);
-  })();
-}
+    (function transition() {
+      d3.transition()
+          .duration(1250)
+          .each("start", function() {
+            title.text(countries[i = (i + 1) % n].name);
+          })
+          .tween("rotate", function() {
+            var p = d3.geo.centroid(countries[i]),
+                r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+            return function(t) {
+              projection.rotate(r(t));
+              c.clearRect(0, 0, width, height);
+              c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill();
+              c.fillStyle = "#f00", c.beginPath(), path(countries[i]), c.fill();
+              c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
+              c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
+            };
+          })
+        .transition()
+          .each("end", transition);
+    })();
+  }
 
-d3.select(self.frameElement).style("height", height + "px");
+  d3.select(self.frameElement).style("height", height + "px");
+
+})
