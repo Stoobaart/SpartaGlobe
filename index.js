fb4aede5244551097bb687027b5e97691fcb6f23
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-03-10 15:17:04 +0000

    node hooked up

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require("express")
+var app = express()
+
+app.use(express.static(__dirname + "/public"))
+
+app.get('/', function(req, res, next) {
+  res.sendFile('/index.html')
+})
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
+
